021ac011cf4fcf0063948b5878f526f5afc954a6
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-05-13 18:08:49 +0000

    conversion of data types

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -7,3 +7,22 @@ console.log(typeof(score));
 let valueinnumber = Number(score)
 console.log(typeof valueinnumber);
 console.log( valueinnumber);
+
+//"33" converted to 33
+// but "33abs" gives NaN
+//true gives 1,and flase gives 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 converted to true and 0 to false
+//"" empty string false
+//"rohan" gives true
+
+let SomeNumber =33
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof(StringNumber));
