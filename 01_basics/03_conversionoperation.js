f890c301e73d3f2dc725270d2052401134f585c9
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-05-13 12:09:59 +0000

    conversions

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,9 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log( valueinnumber);
