a927e39a42ad2db5ceaeaad4c65be0b59fe98ff2
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-05-13 11:46:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)// we are using node js not browser
+
+console.log(3+
+    3)  // this is not a good practice and code readability is our priority
+
+
+console.log("rohan")
+
+
+let name = "rohan"
+let age = 19
+let isloggedin = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone vlae
+// undefinded =>
+// symbol => unique
+
+
+// object
+
+
+
+console.log(typeof"rohan");
+console.log(typeof age);
+console.log(typeof null);// output is object
+console.log(typeof undefined);// output is  undefined
+
+
+
+
+// assignment check ecma documentation and specifications
