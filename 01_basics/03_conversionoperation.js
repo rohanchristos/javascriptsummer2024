bccfa08ff05d893b0593b0d8f6947049dcb3a0fd
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-05-14 18:05:03 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,12 +1,12 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueinnumber = Number(score)
-console.log(typeof valueinnumber);
-console.log( valueinnumber);
+// console.log(typeof valueinnumber);
+// console.log( valueinnumber);
 
 //"33" converted to 33
 // but "33abs" gives NaN
@@ -28,4 +28,24 @@ console.log(StringNumber);
 console.log(typeof(StringNumber));
 
 //*************operations *******************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+// console.log(3+3);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(10%5);
+
+
+let str1 = "hello"
+let str2 = " rohan"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+
 
