bcab4775eaa4c6625fca365cb7a9db0490c7d81f
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-06-18 14:25:43 +0000

    js is dyn.typed

--- 01_basics/datatypessummary.js
+++ 01_basics/datatypessummary.js
@@ -1,3 +1,11 @@
 // primitive
 
-// 7 types : string,number,boolean,null,undefined,symbol
+// 7 types : string,number,boolean,null,undefined,symbol,Bigint
+
+const score = 100
+//JavaScript is a dynamically typed language. This means that you don’t have to declare the type of a variable when you create one, and the type can change at runtime.
+
+// refrence (Non primitive)
+
+// Array, objects , functions
+
