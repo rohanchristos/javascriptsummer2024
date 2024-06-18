dd60e0c05b25a025325f7a082973c0a65d5bdb77
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-06-18 15:17:58 +0000

    datatypes summary

--- 01_basics/datatypessummary.js
+++ 01_basics/datatypessummary.js
@@ -3,9 +3,35 @@
 // 7 types : string,number,boolean,null,undefined,symbol,Bigint
 
 const score = 100
+const scroevalue = 100.3
+const isloggedin = false
+const outsidetemp = null
+let userEmail; // undefined
+
+
+//const id = symbol('123')
+//const anotherId = symbol("123")
+
+//console.log(id === anotherId);
+
+const bigNumber = 647607650601740n
 //JavaScript is a dynamically typed language. This means that you don’t have to declare the type of a variable when you create one, and the type can change at runtime.
 
 // refrence (Non primitive)
 
 // Array, objects , functions
 
+const month = ["jan","feb","may","june","nov"]
+
+let myobj = {
+    name: "rohan",
+    age: 21,
+}
+
+const myfunction = function(){
+    console.log("helloe world");
+}
+
+console.log(typeof bigNumber);
+
+// go to ecma for what output will typeof gives in diffrent input datatypes
