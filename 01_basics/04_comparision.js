cffdfe9d7504a1e6a49ebb97ee4cd7715ccbb451
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-06-18 13:46:22 +0000

    testing and evaluations

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined ==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// ===
+
+console.log("2"===2); // checks the datatype and returns the value in boolean true or false.
+
+// note:we should avoid these type of comparison and we should give priority to clean code.
