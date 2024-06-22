a7791d1574d05939d2cb35e3fd38879bce134c6e
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2024-06-22 14:51:15 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,13 @@
 const name = "rohan"
 const repocount = "55"
 
+// console.log(name + repocount + "value");
+
 console.log(`hello my name is ${name} and my repo count is ${repocount}` );
+
+const gamename = new String('rohanhc')
+
+
+console.log(gamename[1]);
+console.log(gamename[0]);
+console.log(gamename[10]);
